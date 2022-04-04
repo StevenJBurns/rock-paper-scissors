c3922182f9b20ad2a3eff3ee9e91045263fb5cab
StevenJBurns <stevenjburns@msn.com>
2022-04-04 08:49:59 -0600

    feat: use table for scoreboard with styling;  create generic handler for buttons; delete old fieldset code

--- js/handleGameOptionButtonClick.js
+++ js/handleGameOptionButtonClick.js
@@ -1,12 +1,31 @@
+import { RESULTS } from './constants.js'
 import { getPlayerSelection } from './getPlayerSelection.js';
 import { getComputerSelection } from './getComputerSelection.js';
 import { checkForWinner } from './checkForWinner.js';
 
 export const handleGameOptionButtonClick = e => {
   const { currentTarget, currentTarget: { parentNode }} = e;
+  const [player, computer, tie] = RESULTS;
 
   parentNode.setAttribute('data-player-selection', currentTarget.innerHTML);
 
   const winner = checkForWinner(getPlayerSelection(), getComputerSelection());
-  console.log(winner);
+
+  const winsCell = document.querySelector('#cell-wins');
+  const lossCell = document.querySelector('#cell-loss');
+  const tiesCell = document.querySelector('#cell-ties');
+
+  switch (winner) {
+    case player:
+      winsCell.innerHTML = +(++winsCell.innerHTML);
+      return;
+    case computer:
+      lossCell.innerHTML = +(++lossCell.innerHTML);
+      return;
+    case tie:
+      tiesCell.innerHTML = +(++tiesCell.innerHTML);
+      return;
+    default:
+      return;
+  }
 };
--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
 import { renderGameOptionButtons } from './renderGameOptionButtons.js';
 
 renderGameOptionButtons();
-
-// const playButton = document.querySelector('#playButton');
-
-// playButton.addEventListener('click', () => {
-//   const gameHistory = document.querySelector('#gameHistory');
-
-//   const winner = checkForWinner(getPlayerSelection(), getComputerSelection());
-
-//   const newHistoryNode = document.createElement('li');
-//   const newHistoryText = document.createTextNode(winner);
-
-//   newHistoryNode.appendChild(newHistoryText);
-//   gameHistory.appendChild(newHistoryNode);
-// });
